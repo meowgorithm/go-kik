0797a2eefbcf11703e8c594722043b19ef4d6900
Christian Rocha <christian@rocha.is>
2016-04-20 12:58:50 -0400

    Whoops, remove duplicate keyboard structs

--- message.go
+++ message.go
@@ -17,20 +17,6 @@ type Message struct {
 	Keyboards            []Keyboard `json:"keyboards,omitempty"`
 }
 
-// Custom keyboard type
-type Keyboard struct {
-	To        string             `json:"to,omitempty"`
-	Hidden    bool               `json:"hidden,omitempty"`
-	Type      string             `json:"type,omitempty"`
-	Responses []KeyboardResponse `json:"responses,omitempty"`
-}
-
-// Button value type in custom keyboards
-type KeyboardResponse struct {
-	Type string `json:"type,omitempty"`
-	Body string `json:"body,omitempty"`
-}
-
 // Send messages to Kik
 //
 // POST /message
